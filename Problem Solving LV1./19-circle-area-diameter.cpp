d51889f9fd4d545ce6b41dcfb559fceaf8c746aa
Mahmoud <uuiii8998@tuta.io>
2025-09-06 23:16:50 +0300

    Update 19-circle-area-diameter.cpp

---
+++ Problem Solving LV1./19-circle-area-diameter.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+float ReadDiameter()
+{
+	float D;
+  
+	cout << "Please enter diameter D ? " << endl;
+	cin >> D;
+
+	return D;
+}
+
+float CircleAreaByDiameter(float D)
+{
+	const float PI = 3.141592653589793238;
+	float Area = (PI * (pow(D, 2))) / 4;
+
+	return Area;
+}
+
+void PrintResult(float Area)
+{
+	cout << "\n Circle Area By Diameter = " << Area << endl;
+}
+
+int main()
+{
+	PrintResult(CircleAreaByDiameter(ReadDiameter()));
+
+	return 0;
+}
+ 
