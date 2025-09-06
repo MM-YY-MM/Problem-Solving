d37edd2df87754ad6c248e68f136e17f48412fe0
Mahmoud <uuiii8998@tuta.io>
2025-09-07 00:04:04 +0300

    Update 47-loan-installment-months.cpp

---
+++ Problem Solving LV1./47-loan-installment-months.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+float ReadPositiveNumber(string Message)
+{
+	float Number = 0;
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+	return Number;
+}
+
+float TotalMonths(float LoanAmount, float MonthlyInstallment)
+{
+	return (float)LoanAmount / MonthlyInstallment;
+}
+
+int main()
+{
+	float LoanAmount = ReadPositiveNumber("Please Enter Loan Amount?");
+	float MonthlyInstallment = ReadPositiveNumber("Please Enter Monthly Installment?");
+
+	cout << "\nTotal Months to pay = " << TotalMonths(LoanAmount, MonthlyInstallment);
+	cout << endl;
+
+	return 0;
+}
