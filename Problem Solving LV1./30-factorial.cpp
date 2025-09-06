bf2291ee8f5ea8b64d9067debba5cff6896fb595
Mahmoud <uuiii8998@tuta.io>
2025-09-06 23:38:31 +0300

    Update 30-factorial.cpp

---
+++ Problem Solving LV1./30-factorial.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+	int Number;
+
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+	} while (Number < 0);
+	
+	return Number;
+}
+
+int Factorial(int N)
+{
+	int F = 1;
+
+	for (int Counter = N; Counter >= 1; Counter--)
+	{
+		F *= Counter;
+	}
+
+	return F;
+}
+
+int main()
+{
+	cout << Factorial(ReadPositiveNumber("Enter N?"));
+
+	return 0;
+} 
